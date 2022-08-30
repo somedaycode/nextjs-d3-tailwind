d5de5702afd068a59d9a4e3868792ed42dde2fea
Seonkyu Kim <somedaycode@gmail.com>
2022-08-31 01:32:28 +0900

    chore: install prettier and eslint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,10 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <header>
+    <h1>heelo</h1>
+    <Component {...pageProps} />
+  </header>
 }
 
 export default MyApp
