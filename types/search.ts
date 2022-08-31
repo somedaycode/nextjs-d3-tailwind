b717026badfd9ae2ea295d753dd985ddc3b4e0e8
Seonkyu Kim <somedaycode@gmail.com>
2022-08-31 23:52:51 +0900

    feat: 토큰 및 검색 API 응답 타입 추가

---
+++ types/search.ts
@@ -0,0 +1,38 @@
+export type SearchItems = {
+  href: string;
+  items: SearchItem[];
+};
+
+export type SearchItem = {
+  external_urls: {
+    spotify: string;
+  };
+  followers: {
+    href: null | string;
+    total: number;
+  };
+  genres: string[];
+  href: string;
+  id: string;
+  images: [
+    {
+      height: number;
+      url: string;
+      width: number;
+    },
+    {
+      height: number;
+      url: string;
+      width: number;
+    },
+    {
+      height: number;
+      url: string;
+      width: number;
+    },
+  ];
+  name: string;
+  popularity: number;
+  type: 'artist';
+  uri: string;
+};
