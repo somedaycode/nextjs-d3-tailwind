a1ad9a0ef445329720b74b4a570b9da6062d6c73
Seonkyu Kim <somedaycode@gmail.com>
2022-09-01 02:36:15 +0900

    feat: '/network'로 페이지 이동 및 페이지 구현

---
+++ pages/network.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Network: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Network Page</title>
+      </Head>
+    </div>
+  );
+};
+
+export default Network;
