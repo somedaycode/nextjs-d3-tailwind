7181c40b8da3effced8a5d17646b8d9bc881b474
Seonkyu Kim <somedaycode@gmail.com>
2022-09-01 19:48:00 +0900

    fix: 경로 오류로 인한 실패
    * 이 부분은 나중에 확인

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: './out',
-      url: ['http://localhost:3000', 'http://localhost:3000/network'],
+      url: ['http://localhost:3000'],
       numberOfRuns: 2,
     },
     assert: {
