cdb729805442c7bede280c25f9d7ff1183866899
Seonkyu Kim <somedaycode@gmail.com>
2022-09-01 19:08:43 +0900

    feat: lighthouse 점수에 따른 경고 표시

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,7 +5,12 @@ module.exports = {
       url: ['http://localhost:3000'],
       numberOfRuns: 2,
     },
-    assert: {},
+    assert: {
+      assertions: {
+        'categories:performance': ['warn', { minScore: 0.9 }],
+        'categories:accessibility': ['error', { minScore: 0.9 }],
+      },
+    },
     upload: {
       target: 'temporary-public-storage',
     },
