d59fbde51011c6738432e11a4aa505ca59e05eba
Seonkyu Kim <somedaycode@gmail.com>
2022-09-01 19:40:42 +0900

    feat: 개발 모드 빌드에서의 lighthouse ci setup

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 const LOCAL = 'http://localhost:3000/';
 const PROD = 'https://nextjs-d3-tailwind-32erw0xcg-somedaycode.vercel.app/';
 
-const isProduction = process.env.NODE_ENV !== 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   images: {
     loader: 'imgix',
-    path: LOCAL,
+    path: isProduction ? PROD : LOCAL,
   },
 };
