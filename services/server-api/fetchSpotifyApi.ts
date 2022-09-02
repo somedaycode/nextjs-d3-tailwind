912a5b810cb8e5f32feb261f14677c70125a2821
Seonkyu Kim <somedaycode@gmail.com>
2022-09-02 23:59:35 +0900

    chore: 리팩토링을 위한 파일들 제거

--- services/server-api/fetchSpotifyApi.ts
+++
@@ -1,8 +0,0 @@
-import { SearchError, SearchItems } from '@/types';
-import { request } from './request';
-
-export const fetchSpotifyApi = (
-  path: string,
-): Promise<SearchItems | SearchError> => {
-  return request(path).then((res) => res.json());
-};
