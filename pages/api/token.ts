04a2558acd7adc206bba4523df0ffff46181fba7
Seonkyu Kim <somedaycode@gmail.com>
2022-09-03 00:02:56 +0900

    refactor: 토큰을 받아오는 API 함수 파라미터 값 추가 및 로직 변경
    * refresh 옵션을 통해 외부에서 토큰 값을 refresh 할 때의 동작을 조절

--- pages/api/token.ts
+++ pages/api/token.ts
@@ -2,21 +2,36 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 import { SPOTIFY_TOKEN } from '@/consts';
 import { cacheStore } from '@/utils/cache';
-import { fetchSpotifyToken } from '@/services/server-api';
 
-export default async function handler(
+import { apiHandler } from '@/helpers/api/apiHandler';
+
+export default apiHandler({ get: getSpotifyToken });
+
+const ACCOUNT_API = 'https://accounts.spotify.com/api/token';
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+
+export async function getSpotifyToken(
   req: NextApiRequest,
-  res: NextApiResponse<{ status: number; message: string }>,
+  res: NextApiResponse,
+  isRefreshing = false,
 ) {
-  try {
-    await saveSpotifyToken();
-    res.status(200).json({ status: 200, message: 'ok' });
-  } catch (error) {
-    console.error(error);
-  }
-}
+  const savedToken = cacheStore.getCache(SPOTIFY_TOKEN);
+  /**
+   * TODO: 토큰 Invalid 되었을 때 어떻게 처리할지 확인하기
+   */
+  if (savedToken) res.send({ status: 'ok', message: '이미 토큰이 있습니다.' });
+  const body = `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`;
+  const data = await fetch(ACCOUNT_API, {
+    method: 'POST',
+    body,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  });
+  const token = await data.json();
+  cacheStore.setCache(SPOTIFY_TOKEN, token);
 
-export async function saveSpotifyToken() {
-  const data = await fetchSpotifyToken();
-  cacheStore.setCache(SPOTIFY_TOKEN, data);
+  if (isRefreshing) return;
+  res.status(200).send(token);
 }
