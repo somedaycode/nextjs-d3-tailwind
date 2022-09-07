3a7093b06270d2a36af687fc8b11f2f77bbea1e8
Seonkyu Kim <somedaycode@gmail.com>
2022-09-08 00:56:52 +0900

    feat: 연관 아티스트의 node와 link를  추출 후 응답
    * 일단 utils로 관련 함수로 다 빼놓았음
    * 추후 리팩토링 필요
    * 복잡할 수 있어서 주석으로 설명 추가

--- pages/api/artists.ts
+++ pages/api/artists.ts
@@ -2,12 +2,24 @@ import type { Artist, Artists } from '@/types/artists';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 import { apiHandler } from '@/helpers/api/apiHandler';
-import { requestSpotify } from '@/helpers/api/requestSpotify';
 import { errorHandler } from '@/helpers/api/errorHandler';
+import { requestSpotify } from '@/helpers/api/requestSpotify';
 
 import { getSpotifyToken } from './token';
+import {
+  getArtistNodes,
+  getRelatedArtistsById,
+  getSingleArtistNode,
+  setLinksFromArtists,
+  setNodes,
+} from '@/utils/relatedArtists';
 
-export default apiHandler({ post: getArtist });
+import type { Link, Node } from '@/utils/relatedArtists';
+
+export default apiHandler({
+  get: getRelatedArtistsNodeWithLinks,
+  post: getArtist,
+});
 
 async function getArtist(req: NextApiRequest, res: NextApiResponse<Artist[]>) {
   try {
@@ -22,21 +34,49 @@ async function getArtist(req: NextApiRequest, res: NextApiResponse<Artist[]>) {
   }
 }
 
-/**
- * TODO: 연관 아티스트 검색
- * */
-async function getRelatedArtists(
+async function getRelatedArtistsNodeWithLinks(
   req: NextApiRequest,
-  res: NextApiResponse<Artist[]>,
+  res: NextApiResponse<any>,
 ) {
+  const nodes = new Map<string, Node>();
+  const links = new Set<Link>();
+
   try {
-    const id = req.query.id;
-    const query = `artists/${id}/related-artists`;
-    const data: Artists = await requestSpotify(query);
-    const searchArtists = data.artists.items;
-    res.status(200).json(searchArtists);
+    const id = req.query.id as string;
+    const startNode = await getSingleArtistNode(id);
+    /** 검색된 아티스트를 첫 노드에 추가 */
+    nodes.set(id, startNode);
+
+    /** 검색한 아티스트의 연관 아티스트 목록을 불러온 후 Link로 연결 */
+    const relatedArtistsFromStartNode = await getRelatedArtistsById(id);
+    setLinksFromArtists(relatedArtistsFromStartNode, links, id);
+
+    /** 받아온 연관 아티스트 목록을 새로운 노드로 저장 */
+    const nodesFromStartNode = getArtistNodes(relatedArtistsFromStartNode);
+    setNodes(nodesFromStartNode, nodes);
+
+    /**
+     * @description
+     * 각각의 연관 아티스트를 시작으로 위의 로직을 반복
+     */
+    await Promise.all(
+      nodesFromStartNode.map((nextNode) => getRelatedArtistsById(nextNode.id)),
+    ).then((result) =>
+      result.forEach((artistList, i) => {
+        const nextNodeId = nodesFromStartNode[i].id;
+        setLinksFromArtists(artistList, links, nextNodeId);
+        const nextNewNodes = getArtistNodes(artistList) as Node[];
+        setNodes(nextNewNodes, nodes);
+      }),
+    );
+
+    const result = {
+      nodes: [...nodes.values()],
+      links: [...links],
+    };
+
+    res.status(200).json(result);
   } catch (error) {
-    await getSpotifyToken(req, res, true);
     errorHandler('잠깐! 문제가 있어요! 다시 한번 시도해주세요', res);
   }
 }
---
+++ utils/relatedArtists.ts
@@ -0,0 +1,58 @@
+import { requestSpotify } from '@/helpers/api/requestSpotify';
+import { Artist } from '@/types';
+
+export type Node = {
+  id: string;
+  url: string;
+  name: string;
+  value: number;
+};
+
+export type Link = {
+  group: string;
+  source: string;
+  target: string;
+  name: string;
+};
+
+export async function getSingleArtistNode(artistId: string) {
+  const { artists } = await requestSpotify<{ artists: Artist[] }>(
+    `/artists?ids=${artistId}`,
+  );
+  const { id, external_urls, name } = artists[0];
+  return {
+    id,
+    url: external_urls.spotify,
+    name,
+    value: 1,
+  };
+}
+
+export async function getRelatedArtistsById(id: string) {
+  const query = `/artists/${id}/related-artists`;
+  const { artists } = await requestSpotify<{ artists: Artist[] }>(query);
+  return artists;
+}
+export function getArtistNodes(artistList: Artist[]) {
+  return artistList.map(({ id, external_urls, name }) => {
+    return { id, url: external_urls.spotify, name, value: 1 };
+  });
+}
+export function setLinksFromArtists(
+  artistList: Artist[],
+  links: Set<Link>,
+  group: string,
+) {
+  artistList.forEach(({ id, name }) => {
+    links.add({ group, source: group, target: id, name });
+  });
+}
+export function setNodes(nodes: Node[], originalNodes: Map<string, Node>) {
+  nodes.forEach((node) => {
+    if (originalNodes.has(node.id)) {
+      return originalNodes.set(node.id, { ...node, value: node.value + 1 });
+    }
+
+    originalNodes.set(node.id, node);
+  });
+}
