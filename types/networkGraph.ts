ff175e80268176d8956a05115c4323302db19342
Seonkyu Kim <somedaycode@gmail.com>
2022-09-09 00:30:10 +0900

    feat: d3 드래그 이벤트를 위한 타입 추가

--- types/networkGraph.ts
+++ types/networkGraph.ts
@@ -1,3 +1,6 @@
+import type { SimulationNodeDatum } from 'd3';
+import type { D3DragEvent } from 'd3-drag';
+
 export type Node = {
   id: string;
   url: string;
@@ -16,3 +19,17 @@ export type Network = {
   nodes: Node[];
   links: Link[];
 };
+
+export interface NodeDatum extends SimulationNodeDatum {
+  id?: string | number;
+  value?: number | number;
+}
+
+export type Subject = {
+  x: number;
+  fx: number | null;
+  y: number;
+  fy: number | null;
+};
+
+export type DragEvent = D3DragEvent<SVGGElement, typeof SVGGElement, Subject>;
