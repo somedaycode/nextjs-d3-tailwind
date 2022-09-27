f2cc8a3ab395c8c4767a680cfb6643830c40a19c
Seonkyu Kim <somedaycode@gmail.com>
2022-09-28 01:18:41 +0900

    feat: GA - gtag 삽입

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,16 +7,37 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Component {...pageProps} />
-      </Hydrate>
-    </QueryClientProvider>
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          {/* <!-- Google tag (gtag.js) --> */}
+          <Script
+            async
+            src={'https://www.googletagmanager.com/gtag/js?id=G-JB8YF492J6'}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JB8YF492J6'), {
+              page_path: window.location.pathname,
+          };
+        `,
+            }}
+          />
+          <Component {...pageProps} />
+        </Hydrate>
+      </QueryClientProvider>
+    </>
   );
 }
 
